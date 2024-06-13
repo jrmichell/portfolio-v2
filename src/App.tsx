9b91d981c5277ec16ad684b36be3346ec3812cc1
Jack Michell <me@jmich.dev>
2024-06-13 18:29:22 -0500

    fix: Nontransparent navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
+      <Contact />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,6 @@ export default {
   },
   plugins: [daisyui],
   daisyui: {
-    themes: ["nord", "night"],
+    themes: ["winter", "night"],
   },
 };
